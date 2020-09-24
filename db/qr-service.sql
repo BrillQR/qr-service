dab2702614241a757c1270202144e9cea5a57699
Raj Patel <rajp6265@github.com>
2020-09-24 20:37:00 +0530

    DB and Eureka Configuration

---
+++ db/qr-service.sql
@@ -0,0 +1,33 @@
+#DB name will be "qr_service"
+#username postgres 
+#wherever you SET password SET it AS "admin" only
+
+CREATE TABLE public.qr_matadata
+(
+    qr_id character varying(30) NOT NULL,
+    created_by character varying(30) NOT NULL,
+    date_created date,
+    date_updated date,
+    qr_location character varying(100) NOT NULL,
+    qr_scan_count bigint,
+    PRIMARY KEY (qr_id)
+);
+
+CREATE TABLE public.qr_data
+(
+    qr_id character varying(30) NOT NULL,
+    first_name character(15) NOT NULL,
+    last_name character(15),
+    surname character(15),
+    contact bigint,
+    address character varying(100),
+    profession character varying(20),
+    experience character varying(10),
+    education character varying(20),
+    profile_photo character varying(50),
+    email character varying(30),
+    website character varying(30),
+    field_projects character varying(150),
+    business_established_date date,
+    PRIMARY KEY (qr_id)
+);
--- src/main/java/com/brillqr/Application.java
+++ src/main/java/com/brillqr/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
+@EnableDiscoveryClient
 @Configuration
 @SpringBootApplication
 public class Application {
